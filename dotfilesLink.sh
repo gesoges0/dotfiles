1ac10f6612adda8ffc904a8085d190f621cff49e
gesoges0 <elasticnet1995@gmail.com>
2024-01-12 23:05:47 +0900

    fix

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -10,4 +10,3 @@ ln -sf ~/src/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/src/dotfiles/.config/Code/User/settings.json ~/.config/Code/User/settings.json
 ln -sf ~/src/dotfiles/.delta/themes.gitconifg ~/.delta/themes.gitconfig
 ln -sf ~/src/dotfiles/.config/bat/config ~/.config/bat/config
-# bat --config-file
